0604cf5c1ac821848535c6beb118c54b4dd6eaf7
Julian Dunne <G00267940@gmit.ie>
2020-02-09 22:21:31 +0000

    Weekly Task 3 - reverse string

---
+++ secondstringreverse.py
@@ -0,0 +1,11 @@
+# This program takes a string
+# and returns every second
+# value in reverse order.
+
+messagerequest = input("Please enter a message: ")
+
+print (messagerequest[-1:0:-2])
+
+# Note: This is the 2nd time I have pushed this task 
+# to github. The first time was before I set up my 
+# GMIT repository.
