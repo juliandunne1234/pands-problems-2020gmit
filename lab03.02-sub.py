a5ecbba3e1223e8bdab89e57ee36e4c2fe241ddc
Julian Dunne <G00267940@gmit.ie>
2020-02-10 22:20:50 +0000

    Topic03 lab03.02-sub.py

---
+++ lab03.02-sub.py
@@ -0,0 +1,10 @@
+# Program that reads in two numbers
+# and subtracts one number from the other.
+
+frst_nmbr = int(input("Please enter the first number: "))
+
+scnd_nmbr = int(input("Please enter the second number: "))
+
+subtract = frst_nmbr - scnd_nmbr
+
+print ("The difference between the numbers is:", subtract)
