415b216997983679d34d7d00d9a43aedbf267122
Julian Dunne <G00267940@gmit.ie>
2020-02-11 06:59:49 +0000

    Topic03 lab03.09-constructor.py

---
+++ lab03.09-constructor.py
@@ -0,0 +1,12 @@
+# Create Dictionary Object
+# using a constructor.
+
+person = {
+    'first_name': 'Julian',
+    'last_name': 'Dunne',
+    'age': 31,
+    'college': 'GMIT',
+    'studying': 'Data Analysys',
+}
+for key, value in person.items():
+    print(key + ":", value)
