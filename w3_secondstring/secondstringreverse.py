c47bc27a33748990798442accfeae8722f10c57e
Julian Dunne <G00267940@gmit.ie>
2020-02-18 22:31:36 +0000

    update folders

--- w3_secondstring/secondstringreverse.py
+++ w3_secondstring/secondstringreverse.py
@@ -5,7 +5,3 @@
 messagerequest = input("Please enter a message: ")
 
 print (messagerequest[-1:0:-2])
-
-# Note: This is the 2nd time I have pushed this task 
-# to github. The first time was before I set up my 
-# GMIT repository.
